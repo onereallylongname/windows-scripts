3281ba768423b7b02dc9ef445d8f646e0a430ebc
onereallylongname <antonioalmeida2099@gmail.com>
2024-10-04 09:29:32 +0100

    Refactor scripts to accept a query value

--- scripts/gt.bat
+++ scripts/gt.bat
@@ -1,41 +1,41 @@
  @echo off
 set searchdir=%HOME%
+set fzfquery=""
 
 if "%1" == "-p" goto open_programs
 if "%1" == "-t" goto open_tasks
 if "%1" == "-tmp" goto open_tmp
-if "%1" == "" goto next_arg
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`fzf --query=%1 --walker=dir --walker-root=%searchdir% --preview="tree {} /A /F"`) DO (
-  SET var=%%F
-)
-cd %var%
-goto end
+if "%1" NEQ "" goto set_quey_1
 
 :next_arg
-  FOR /F "tokens=* USEBACKQ" %%F IN (`fzf --walker=dir --walker-root=%searchdir% --preview="tree {} /A /F"`) DO (
-    SET var=%%F
+  if %fzfquery% NEQ "" (
+    FOR /F "tokens=* USEBACKQ" %%F IN (`fzf --query=%fzfquery% --walker=dir --walker-root=%searchdir% --preview="tree {} /A /F"`) DO (
+      cd %%F
+    )
+  ) else (
+    FOR /F "tokens=* USEBACKQ" %%F IN (`fzf --walker=dir --walker-root=%searchdir% --preview="tree {} /A /F"`) DO (
+      cd %%F
+    )
   )
-  cd %var%
-  goto end
-
-:next_arg
-  FOR /F "tokens=* USEBACKQ" %%F IN (`fzf --walker=dir --walker-root=%searchdir% --preview="tree {} /A /F"`) DO (
-    SET var=%%F
-  )
-  cd %var%
   goto end
 
 :open_programs
   set searchdir="%HOME%\Projects"
+  if "%2" NEQ "" set fzfquery="%2"
   goto next_arg
 
 :open_tasks
   set searchdir="%HOME%\Tasks"
+  if "%2" NEQ "" set fzfquery="%2"
   goto next_arg
 
 :open_tmp
   set searchdir="%HOME%\tmp"
+  if "%2" NEQ "" set fzfquery="%2"
+  goto next_arg
+
+:set_quey_1
+  set fzfquery="%1"
   goto next_arg
 
 :end
